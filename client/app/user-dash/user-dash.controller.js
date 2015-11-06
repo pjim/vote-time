0be944a7b039e17f956bb330cf0553bd1799ea7d
pjim <paul.k.james@googlemail.com>
2015-11-06 11:14:23 +0000

    Add and remove poll options with button

--- client/app/user-dash/user-dash.controller.js
+++ client/app/user-dash/user-dash.controller.js
@@ -2,10 +2,27 @@
 
 angular.module('voteTimeApp')
   .controller('UserDashCtrl', function ($scope) {
-      $scope.addPoll = function(){
-          for(option in options,function(){
+  //    $scope.addPoll = function(){
+        //  for(option in options,function(){
             //turn a variable number of options into an array for sending
-          });
-          $http.post('api/polls/', {question:pollQuestion,options:[optionArray]})  
-      }
+        //  });
+        //  $http.post('api/polls/', {question:pollQuestion,options:[optionArray]})
+      //}
+      $scope.options = [
+        'option1',
+        'option2'
+      ];
+      $scope.addOption = function(event){
+        event.preventDefault();
+        var optNum = $scope.options.length +1;
+        $scope.options.push('option' + optNum);
+      };
+      $scope.removeOption = function(event){
+        event.preventDefault();
+        if($scope.options.length > 2){
+          $scope.options.pop();
+        }else{
+          alert('You must have at least two options in your poll');
+        }
+      };
   });
