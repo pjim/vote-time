4b36ec07ed4421af26c960aecdaca257a568f6c7
pjim <paul.k.james@googlemail.com>
2015-11-12 13:06:32 +0000

    fix main page offset

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -8,36 +8,8 @@ angular.module('voteTimeApp')
 
     $scope.chartData = pollGetter.getChartObjects();
 
-    //  $http.get('api/polls').success(function(resp){
-     //
-    //     resp.forEach(function(value){
-    //       var voteCount = 0;
-    //        value.options.forEach(function(val){
-    //           voteCount += val.votes;
-    //        });
-    //         if(voteCount > 0){
-    //           var chartObject = {};
-    //           chartObject.options = {};
-    //           chartObject.options.chart = {type:'column'};
-    //           chartObject.xAxis = {}  ;
-    //           chartObject.xAxis.categories = [];
-    //           chartObject.xAxis.title = {text:'Poll Options'};
-    //           chartObject.yAxis = {};
-    //           chartObject.yAxis.title = {text:'Votes'};
-    //           chartObject.yAxis.minTickInterval = 1;
-    //           chartObject.series = [{data:[]}];
-    //           chartObject.series[0].name = 'Number of Votes';
-    //           chartObject.title = {text:value.question};
-    //           value.options.forEach(function(option){
-    //              chartObject.xAxis.categories.push(option.optionName);
-    //              chartObject.series[0].data.push(option.votes);
-    //           });
-    //           $scope.chartData.push(chartObject);
-    //         }
-    //     });
 
-  //   });
-     $scope.seePolls = false;
+     $scope.seePolls = false ;
 
      $scope.showPolls = function(){
        if($scope.seePolls === true){
@@ -57,4 +29,5 @@ angular.module('voteTimeApp')
        }
      };
 
+     $scope.isLoggedIn = Auth.isLoggedIn;
   });
